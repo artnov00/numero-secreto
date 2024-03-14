6e793e57a3de720523fcb122c52de10e7f953c88
Arthur Novais <n.arthurnds@gmail.com>
2024-03-13 21:49:48 -0300

    Deixando o jogo mais facil

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [] // usando arrays para criar listas
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
